759c9af40a72dbc59c41d67627a83bc7febbbb78
david <davidtoledo87@hotmail.com>
2020-11-16 17:30:29 +1100

    Monday 16th Nov

--- js/main.js
+++ js/main.js
@@ -1,5 +1,66 @@
 console.log("this will run the game");
 
+const variables = {
+
+}
+
+
+let player;
+
+$("#buttonX").on("click",function(){
+  console.log("X pressed");
+  player="x"
+  console.log("its the turn for",player);
+})
+
+$("#buttonO").on("click",function(){
+  console.log("O pressed");
+  player="o"
+  console.log("its the turn for",player);
+})
+
+
 $("#box1").on("click",function(){
     console.log("box1 pressed");
+    if(player=="x" && $("#box1 p").html() !="O"){
+    $("#box1 p").html("X");
+    player="o";
+    console.log("now its the turn for:",player);
+  } else if (player=="o" && $("#box1 p").html() !="X"){
+    $("#box1 p").html("O");
+    player="x";
+    console.log("now its the turn for:",player);
+  } else {
+    console.log("try somewhere else");
+  }
+})
+
+$("#box2").on("click",function(){
+    console.log("box2 pressed");
+    if(player=="x" && $("#box2 p").html() !="O"){
+    $("#box2 p").html("X");
+    player="o";
+    console.log("now its the turn for:",player);
+  } else if (player=="o" && $("#box2 p").html() !="X"){
+    $("#box2 p").html("O");
+    player="x";
+    console.log("now its the turn for:",player);
+  } else {
+    console.log("try somewhere else");
+  }
+})
+
+$("#box3").on("click",function(){
+    console.log("box3 pressed");
+    if(player=="x" && $("#box3 p").html() !="O"){
+    $("#box3 p").html("X");
+    player="o";
+    console.log("now its the turn for:",player);
+  } else if (player=="o" && $("#box3 p").html() !="X"){
+    $("#box3 p").html("O");
+    player="x";
+    console.log("now its the turn for:",player);
+  } else {
+    console.log("try somewhere else");
+  }
 })
